1a61aaaa48b0957745a4c270d02c40883a255c12
oh jinseok <ojs258@naver.com>
2022-11-04 17:59:56 +0900

    2022-11-04

--- class2/12. 통계학.py
+++ class2/12. 통계학.py
@@ -1,16 +1,23 @@
 import sys
+from collections import Counter
 
 num = list(int(sys.stdin.readline())
 for i in range(int(sys.stdin.readline())))
 
-num1 = sorted(set(num))
-num2 = []
-print(sum(num) / len(num))
-print(num1[len(num1)//2])
+num.sort()
+c_num = Counter(num).most_common()
 
-for i in len(num1):
-    num2[i] = [num1[i]]
-
-print(num2[])
-
-print(abs(max(num) - min(num)))
+#평균값
+print(int(round(sum(num) / len(num),0)))
+#중앙값
+print(sorted(num)[len(num)//2])
+#최빈값
+if len(c_num) > 1:
+        if c_num[0][1] == c_num[1][1]:
+            print(c_num[1][0])
+        else:
+            print(c_num[0][0])
+else:
+    print(c_num[0][0])
+#범위
+print(max(num)-min(num))
---
+++ class2/13. 카드2.py
@@ -0,0 +1,7 @@
+import sys
+
+n = list(range(1,int(sys.stdin.readline())))
+
+
+del n[i]
+print(n)
