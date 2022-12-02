578e4ca6053452ddc0c5c3d82d8129b9209519a2
oh jinseok <ojs258@naver.com>
2022-12-02 17:09:52 +0900

    2022-12-02

--- class3/3. Z.py
+++ class3/3. Z.py
@@ -1,13 +1,27 @@
 import sys
 
-N, r, c = map(int,sys.stdin.readline().split())
+dx=[0,1,0,1]
+dy=[0,0,1,1]
 
-dx=[1,-1,1,-1]
-dy=[0,1,0,-1]
-a, b = 0, 0
-cnt = 0
-while cnt < 2**(2*N):
-    for i in range(4):
-        a += dx[i]; b += dy[i]
-        if a == c and b == 
-        cnt += 1
+n, y, x = map(int,sys.stdin.readline().split())
+ans = 0
+while n > 1:
+    n -= 1
+    size = 2 ** n
+    if x < size and y < size:#2사분면
+        pass
+    elif x < size and y >= size:#3사분면
+        ans += (size ** 2) * 2
+        y -= size
+    elif x >= size and y < size:#1사분면
+        ans += size ** 2
+        x -= size
+    elif x >= size and y >= size:#4사분면
+        ans += (size ** 2) * 3
+        x -= size; y -= size
+
+for i in range(4):
+    if x == dx[i] and y == dy[i]:
+        ans += i
+
+print(ans)
