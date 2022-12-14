6b3d67b0ff3ade03c5cce24dca8b711c25969013
oh jinseok <ojs258@naver.com>
2022-12-14 17:58:55 +0900

    2022-12-14

--- class3/6. 케빈 베이컨의 6단계 법칙.py
+++ class3/6. 케빈 베이컨의 6단계 법칙.py
@@ -1,6 +1,25 @@
 import sys
 
+def dfs(graph,start,point):
+    cnt = 0
+    visit=[]
+    for i in point:
+        stk=[start]
+        while stk:
+            n = stk.pop(0)
+            if n not in visit:
+                if i in graph[n]:
+                    cnt =+ 1
+                    break
+                visit.append(n)
+                stk += set(graph[n]) - set(visit)
+    return cnt
+
+def dfs2(stk,cnt,graph,point)
+
+
 node, line = map(int,sys.stdin.readline().split())
+nodes = list(range(1,node+1))
 lines = []
 graph = {}
 for i in range(node):
@@ -17,8 +36,9 @@ for i in range(1,node+1):
     if tmp:
         graph[i] =  sorted(tmp)
 
-ans = {}
-for i in range(1,node+1):
-    ans[i] = len(graph[i])
+ans = []
+for i in nodes:
+    tmp = list(set(nodes) - set([i]))
+    ans.append(dfs(graph,i,tmp))
 
-print(max(ans.values()))
+print(ans)
